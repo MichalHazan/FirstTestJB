6cf9a8f25c7446aa23dd3eef2380097f1d7ad9fb
Michal Hazan <michalhazansnt@gmail.com>
2021-10-26 11:28:20 +0300

    test

---
+++ index.js
@@ -0,0 +1,68 @@
+const AllProductsDiv = document.querySelector(".AllProductsDiv") //הדיב של כל המוצרים
+const AddProductBtn = document.querySelector(".AddProduct") //  כפתור הוספת המוצרים
+const Pname = document.querySelector('#Pname') //האינפוט שמכיל את שם המוצר
+const Pprice = document.querySelector('#Pprice') //האינפוט שמכיל את מחיר המוצר
+const Pcatagory = document.querySelector('#Pcatagory') //האינפוט שמכיל את קטגוריית המוצר
+const Ppic = document.querySelector('#Ppic') //האינפוט שמכיל את תמונת המוצר
+Pcatagory.value = "" //נתחיל עם זה שלא יופיע קטגוריה בבחירת המוצר כך שהמשתמש יחלץ ואז יראה מהן הקטגרויות
+
+//----יצירת אובייקט מערך מוצרים----
+const Parr = [];
+
+//לחיצת הכפתור דרכו מוסיפים מוצרים
+AddProductBtn.addEventListener('click', function (e) {
+    //קודם נבדוק שלא השאירו שדות ריקים
+    if (Pname.value == "" || Pprice.value == "" || Pcatagory.value == "" || Ppic.value == "") {
+        alert("יש למלא את כל השדות")
+
+    } else if (!Ppic.value.startsWith("http")) {
+        alert("כתובת תמונה שגויה")
+    } else {
+
+        const ProductDiv = document.createElement('div') //יצירת דיב לכל מוצר
+        const ProductName = document.createElement('h2') //   שם המוצר
+        const ProductPrice = document.createElement('h3') //   מחיר המוצר
+        const ProductPic = document.createElement('img') //   תמונת המוצר
+        const ProductCat = document.createElement('h4') //   קטגוריית המוצר
+
+        ProductDiv.className = "ProductDiv"
+
+        //יצירת כפתור מחיקה--------------------------
+        const delbtn = document.createElement('button')
+        delbtn.textContent = "Remove Product"
+        delbtn.className = "delbtn"
+        delbtn.addEventListener('click', function (ev) {
+            ev.target.parentElement.remove()
+        })
+        //----------------------------------------
+
+        ProductName.textContent = Pname.value
+        ProductPrice.textContent = Pprice.value + "$"
+        ProductCat.textContent = "Catagory: " + Pcatagory.value
+        ProductPic.src = Ppic.value
+
+        ProductDiv.appendChild(ProductPic)
+        ProductDiv.appendChild(ProductName)
+        ProductDiv.appendChild(ProductPrice)
+        ProductDiv.appendChild(ProductCat)
+        ProductDiv.appendChild(delbtn)
+
+        AllProductsDiv.appendChild(ProductDiv)
+
+        //---הוספה למערך-----
+        Parr.push({
+            ProName: Pname.value,
+            ProPrice: Pprice.value,
+            ProCat: Pcatagory.value,
+            Pimg: Ppic.value
+        })
+        console.log(Parr);
+        //--------------------
+
+        //-------איפוס הטופס------------------
+        Pname.value = ""
+        Pprice.value = ""
+        Pcatagory.value = ""
+        Ppic.value = ""
+    }
+})
